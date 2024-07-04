44128758f608d46db53a135943c21a662e392e7c
Muhammad Rizky <rizkymuhammad490@gmail.com>
2024-07-04 22:37:25 +0700

    Added Profile

---
+++ app/src/main/java/com/muriz/composetutorial/ui/presentation/profile/profileComponent/ButtonProfile.kt
@@ -0,0 +1,75 @@
+package com.muriz.composetutorial.ui.presentation.profile.profileComponent
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PersonAdd
+import androidx.compose.material.icons.outlined.PersonAdd
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ButtonProfile(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(5.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(7.dp),
+            modifier = modifier.size(width = 165.dp, height = 40.dp),
+            colors = ButtonDefaults.buttonColors(Color.LightGray)
+        ) {
+            Text(text = "Edit Profil", textAlign = TextAlign.Center, fontSize = 18.sp)
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(7.dp),
+            modifier = modifier.size(width = 165.dp, height = 40.dp),
+            colors = ButtonDefaults.buttonColors(Color.LightGray)
+        ) {
+            Text(text = "Bagikan Profil", textAlign = TextAlign.Center, fontSize = 18.sp)
+        }
+        Box(
+            modifier = modifier
+                .size(40.dp)
+                .background(color = Color.LightGray, RoundedCornerShape(7.dp)),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.PersonAdd,
+                contentDescription = "person add",
+                modifier = modifier
+                    .size(30.dp)
+            )
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun test() {
+    ButtonProfile()
+}
--- app/src/main/java/com/muriz/composetutorial/ui/presentation/profile/profileComponent/DetailProfile.kt
+++ app/src/main/java/com/muriz/composetutorial/ui/presentation/profile/profileComponent/DetailProfile.kt
@@ -1,12 +1,58 @@
 package com.muriz.composetutorial.ui.presentation.profile.profileComponent
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.muriz.composetutorial.R
 
 @Composable
 fun DetailProfile(modifier: Modifier = Modifier) {
-    Row {
-
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(5.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        AsyncImage(
+            model = R.drawable.me,
+            contentDescription = "image profile",
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                .clip(CircleShape)
+                .size(70.dp)
+        )
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "1")
+            Text(text = "Postingan")
+        }
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "225")
+            Text(text = "pengikut")
+        }
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "218")
+            Text(text = "mengikuti")
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun test() {
+    DetailProfile()
 }
